0a30351aba89d211180f510f385f8805eb1f498a
Hardik Dalwadi <hardik.dalwadi@gmail.com>
2020-12-01 22:48:02 +0000

    - Add: Generate Ansible Inventory for OpenStack

--- inventory-overcloud.sh
+++ inventory-overcloud.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-source ~/stackrc
+### Global config ###
+OSP_UC_CONFIG=stackrc ## OpenStack undercloud config file
 
-#Dynamically generate static inventory (yaml format) for overcloud
-#exec /usr/bin/tripleo-ansible-inventory --static-yaml-inventory $(openstack stack list -f value -c "Stack Name")StaticIventory.yaml --stack $(openstack stack list -f value -c "Stack Name")
+## Import OpenStack undercloud config file 
+source ~/$OSP_UC_CONFIG
 
-#Dynamically generation nventory (json format) to Ansible (Example: ansible -i $0 all -m ping) 
-exec /usr/bin/tripleo-ansible-inventory --stack $(openstack stack list -f value -c "Stack Name") --list
+## Get OpenStack undercloud single / multiple stacks names
+STACK_INPUT=$(openstack stack list -f value -c "Stack Name"  --sort-column "Stack Name" | tr '\n ' ',')
+
+## Generate file name from OpenStack undercloud single / multiple stacks names
+STACK_NAME=$(openstack stack list -f value -c "Stack Name"  --sort-column "Stack Name" | tr '\n' '-')
+
+## Dynamically generate static inventory (yaml format) for overcloud
+/usr/bin/tripleo-ansible-inventory --static-yaml-inventory ~/${STACK_NAME}_StaticIventory.yaml --stack ${STACK_INPUT}
+
+## Dynamically generate inventory (json format) to ansible (Example: ansible -i $0 all -m ping) 
+exec /usr/bin/tripleo-ansible-inventory --stack ${STACK_INPUT} --list
